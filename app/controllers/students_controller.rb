1301828f2e8b995d5fe05105a4528b50d14e3646
Zachary Lehmann <zlehmann@gmail.com>
2019-01-07 20:34:09 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
   end
 
   def new
-    
+
   end
 
 end
