9be80d9b6f04f22f999d8c24ffd8888b333d8718
Zachary Lehmann <zlehmann@gmail.com>
2019-01-07 20:33:23 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :students, only: :index
-  get '/students/:id', to: 'students#show', as: 'student'
   get '/students/new', to: 'students#new', as: 'new_student'
+  get '/students/:id', to: 'students#show', as: 'student'
 end
